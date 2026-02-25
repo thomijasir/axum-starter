ad308c6d0ae69fbe7221d23d91d4c72487cc5136
Thomi Jasir <thomi.jasi1@uobgroup.com>
2026-02-25 10:13:10 +0800

    chore(ignore): update ignore files and clean project

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use std::sync::Arc;
-
 use axum_starter::{config, models::AppState, server::AppServer, services::DBSqlite};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
   let env = config::load_environment();
   config::init_logging(&env);
   config::ensure_directories(&env);
-
-  tracing::info!(mode = %env.mode, port = env.port, "SERVER_START");
-
   // Create DB connection pool
   let db = DBSqlite::new(&env.database_url).expect("DATABASE_POOL_FAILURE");
   // Run pending migrations
   db.run_migrations().expect("DATABASE_MIGRATION_FAILURE");
+  // Log Start
+  tracing::info!(mode = %env.mode, port = env.port, "SERVER_START");
   // Create App State
   let app_state = Arc::new(AppState { env, db });
 
